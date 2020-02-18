66ecfdecc832eb0edbd2a0706b0b241f76cb56d7
부섭 조 <egm7126@naver.com>
2020-02-19 00:49:35 +0900

    가명 자료형 만들기 실습함

--- 소스1.cpp
+++ 소스1.cpp
@@ -5,4 +5,11 @@ int main
 	using namespace std;
 
 	cout << "hello world, this is test of plug-in of visual studio" << endl;
+
+	typedef newint int;
+
+	newint a = 1;
+	newint b = 2;
+
+	cout << "a+b= " << a + b << endl;
 }
